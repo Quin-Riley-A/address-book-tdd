3ef35cffe3eae1354464c7828bf93b3e1aacf62c
Quin-Riley-A <quin.asselin1@gmail.com>
2022-09-11 14:47:09 -0400

    add delectContact functionality Co-authored-by: Peter Philavanh <efl2ice89@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 //Business Logic for AdressBook
-function AdressBook() {
+function AddressBook() {
   this.contacts = {};
   this.currentId = 0;
 }
@@ -12,6 +12,21 @@ AddressBook.prototype.assignId = function() {
 AddressBook.prototype.addContact = function(contact) {
   contact.id = this.assignId();
   this.contacts[contact.id] = contact;
+};
+
+AddressBook.prototype.findContact = function(id) {
+  if (this.contacts[id] !== undefined) {
+    return this.contacts[id];
+  }
+  return false;
+};
+
+AddressBook.prototype.deleteContact = function(id) {
+  if (this.contacts[id] === undefined) {
+    return false;
+  }
+  delete this.contacts[id];
+  return true;
 }
 
 //Business Logic for Contacts
@@ -21,7 +36,7 @@ function Contact(firstName, lastName, phoneNumber) {
   this.phoneNumber = phoneNumber;
 }
 
-Concact.prototype.fullName = function() {
+Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
 };
 
