f17d9bd0866452adbd8572da4b3ea658c85f2dc0
Quin-Riley-A <quin.asselin1@gmail.com>
2022-09-11 14:25:09 -0400

    continue building functionality for AddressBook Co-authored-by: Peter Philavanh <efl2ice89@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,20 @@
-//Contact constructor
+//Business Logic for AdressBook
 function AdressBook() {
-  this.contacts = {}
+  this.contacts = {};
+  this.currentId = 0;
 }
 
+AddressBook.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
+};
+
+AddressBook.prototype.addContact = function(contact) {
+  contact.id = this.assignId();
+  this.contacts[contact.id] = contact;
+}
+
+//Business Logic for Contacts
 function Contact(firstName, lastName, phoneNumber) {
   this.firstName = firstName;
   this.lastName = lastName;
@@ -12,3 +24,4 @@ function Contact(firstName, lastName, phoneNumber) {
 Concact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
 };
+
