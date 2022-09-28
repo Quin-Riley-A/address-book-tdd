41e45162f8444328db0a6b0cf307ba64ca37b4ef
Quin-Riley-A <quin.asselin1@gmail.com>
2022-09-27 22:24:49 -0400

    produce good fail for Addressbook.test test 4 Co-Authored-by: Grace Lee <gracexsnoopy@gmail.com> Co-authored-by Athea DeLing <almdeling@gmail.com> co-authored-by: Niles O'Neil <nilesoneil@gmail.com>

--- __tests__/addressbook.test.js
+++ __tests__/addressbook.test.js
@@ -1,4 +1,5 @@
 //import test from "node:test";
+//import test from "node:test";
 import AddressBook from "../src/addressbook";
 import Contact from '../src/contact.js';
 
@@ -17,5 +18,17 @@ describe('AddressBook', () => {
     const contact1 = new Contact("Sonya", "Jamalungma", "603-555-2000", "sJam@ymail.com");
     addBook.addContact(contact1);
     expect(addBook.contacts[1]).toEqual(contact1);
+  });
+
+  test('should return the contact ID for contact1', () => {
+    const contact1 = new Contact("Sonya", "Jamalungma", "603-555-2000", "sJam@ymail.com");
+    addBook.addContact(contact1);
+    expect(addBook.findContact(1)).toEqual(contact1);
+  });
+
+  test('return false for a non-existing contact ID', () => {
+    const contact1 = new Contact("")
+    addBook.addContact(contact1);
+    expect(addBook.findContact(2)).toEqual(false);
   });
 });
--- src/addressbook.js
+++ src/addressbook.js
@@ -1,3 +1,5 @@
+import { tSImportEqualsDeclaration } from "@babel/types";
+
 //Business Logic for AdressBook
 export default function AddressBook() {
   this.contacts = {};
@@ -18,7 +20,7 @@ AddressBook.prototype.findContact = function(id) {
   if (this.contacts[id] !== undefined) {
     return this.contacts[id];
   }
-  return false;
+  //return false;
 };
 
 AddressBook.prototype.deleteContact = function(id) {
