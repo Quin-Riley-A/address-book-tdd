143c29f1b3a44b8df3c39ae225ba6226c0fe5ad2
Quin-Riley-A <quin.asselin1@gmail.com>
2022-09-27 22:03:41 -0400

    DRY code in Addressbook.test
    Co-Authored-by: Grace Lee <gracexsnoopy@gmail.com>
    Co-authored-by Athea DeLing <almdeling@gmail.com>
    co-authored-by: Niles O'Neil <nilesoneil@gmail.com>

--- __tests__/addressbook.test.js
+++ __tests__/addressbook.test.js
@@ -3,14 +3,17 @@ import AddressBook from "../src/addressbook";
 import Contact from '../src/contact.js';
 
 describe('AddressBook', () => {
+  let addBook;
+
+  beforeEach(() => {
+    addBook = new AddressBook();
+  });
 
   test('should create a new address book', () => {
-    const addBook = new AddressBook();
     expect(addBook.currentId).toEqual(0);
   });
 
   test('should populate the Addressbook with contact1', () => {
-    const addBook = new AddressBook();
     const contact1 = new Contact("Sonya", "Jamalungma", "603-555-2000", "sJam@ymail.com");
     addBook.addContact(contact1);
     expect(addBook.contacts[1]).toEqual(contact1);
